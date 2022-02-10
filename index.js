c2a7e315b58b395037c76b54e5fd07f218340a47
aidenakash <96476154+aidenakash@users.noreply.github.com>
2022-02-10 02:44:18 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/*const {test,data}=require("./modules/test1")
+console.log(data)
+test()*/
+/*const os=require("os")
+console.log(os.uptime())
+console.log(os.type())
+console.log(os.release())
+console.log(os.totalmem())
+console.log(os.freemem())
+console.log(__dirname)*/
+
